c6d5613e668a82912467e57b34cc9ee9587db1e8
root <root@localhost.localdomain>
2019-10-25 12:25:43 +0300

    Javascript

---
+++ test/index.js
@@ -0,0 +1,13 @@
+
+function myfunction(){
+    var text = "My First JS Program"
+    document.getElementById("intro").innerHTML=text
+
+}
+function myheading(){
+    var head = "This is the heading"
+    document.getElementById("test").innerHTML=head
+}
+function date() {
+    document.getElementById("demo").innerHTML = date()
+}
